0aeef5c9e2db364bb0c5a8b195facf4960196390
luisalfredoestigarribiasosa85 <luisalfredoestigarribiasosa85@gmail.com>
2025-05-24 17:31:01 -0300

    added recent changes

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -23,11 +25,10 @@ app.set("layout", "./layouts/layout") // not at views root
  * Routes
  *************************/
 app.use(static)
+app.use("/inv", inventoryRoute)
 
 //Index Route
-app.get("/", (req, res) => {
-  res.render("index", { title: "Home" })
-})
+app.get("/", baseController.buildHome)
 
 /* ***********************
  * Local Server Information
